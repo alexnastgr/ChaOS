cf786b7ee8c26ca554d4a7f64a487f833172b280
alexnastgr <alexanstgr@gmail.com>
2026-03-27 23:38:44 +0200

    dynamic background

--- src/components/dockbar/DockApp.tsx
+++ src/components/dockbar/DockApp.tsx
@@ -1,21 +1,20 @@
 import type { ReactNode } from "react";
-
+import useTheme from "@/hooks/useTheme";
 interface Props {
   tooltip: string;
-  children:ReactNode;
+  children: ReactNode;
   isOpen: boolean;
 }
 
-export default function DockApp({ tooltip,children,isOpen }: Props) {
+export default function DockApp({ tooltip, children, isOpen }: Props) {
+  const { darkMode } = useTheme();
 
-  const state = isOpen ? "bg-blue-500" : "bg-black"
+  const state = isOpen ? "bg-blue-500" : "bg-black";
   return (
     <div className="group relative flex flex-col items-center">
-      <div className="tooltip">
-        {tooltip}
-      </div>
+      <div className="tooltip">{tooltip}</div>
 
-      <div className={`dockapp ${state}`}>
+      <div className={`dockapp ${darkMode == "dark" ? "dark" : "light"} ${state}`}>
         {children}
       </div>
     </div>
